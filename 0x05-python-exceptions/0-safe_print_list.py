f7988a8fcbc7d2b9e8cb9a6500156f8e3a50dba0
DORCAS AUMA <dorcasauma89@gmail.com>
2024-01-22 19:13:43 +0300

    KK

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[]):
-    x = 0
-    my_elements = ""
-    for i in my_list:
-        x += 1
-        my_elements += str(i)
-    print("x = {}".format(x))
-    print("my_elements = {}".format(my_elements))
+    try:
+        x = 0
+        my_elements = ""
+        for i in my_list:
+            x += 1
+            my_elements += str(i)
+        print("x = {}".format(x))
+        print("my_elements = {}".format(my_elements))
+    except IndexError:
+        print()
