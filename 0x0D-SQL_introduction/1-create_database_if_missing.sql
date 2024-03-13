3933bd017dc8f83a64ba77cdaa6188dfbc72d91e
DORCAS AUMA <dorcasauma89@gmail.com>
2024-03-13 19:14:37 +0300

    DATABASE CREATION

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- Create database hbtn_0c_0 if it does not already exist
+
+-- Check if the database exists in the information_schema.SCHEMATA table
+-- If it does not exist, create the database
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
