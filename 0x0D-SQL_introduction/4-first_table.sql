f67dc98df1d3297f5c8b952bbe8ab24bdd79828d
DORCAS AUMA <dorcasauma89@gmail.com>
2024-03-13 19:28:24 +0300

    create table correction

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,10 +1,6 @@
 -- Create table first_table in the current database
 
--- Parameter for the table name
 SET @table_name = 'first_table';
-
--- Check if the table exists in the current database
--- If it does not exist, create the table
 CREATE TABLE IF NOT EXISTS @table_name (
     id INT,
     name VARCHAR(256)
