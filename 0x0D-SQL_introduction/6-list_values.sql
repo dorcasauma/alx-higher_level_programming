cf93a199dd9c3136194ed90229f541a50acbbae5
DORCAS AUMA <dorcasauma89@gmail.com>
2024-03-13 19:42:18 +0300

    rows of the table

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,8 @@
+-- List all rows of the table first_table
+
+-- Parameter for the database name
+SET @database_name = 'hbtn_0c_0';
+
+-- Query to select all rows from the table
+SELECT *
+FROM @database_name.first_table;
