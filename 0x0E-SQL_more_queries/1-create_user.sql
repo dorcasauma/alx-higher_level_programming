47920a61da2092d208a893c76dc4b76416398b62
DORCAS AUMA <dorcasauma89@gmail.com>
2024-03-13 21:29:56 +0300

    PRIVELEGE

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- Create MySQL user user_0d_1 with all privileges
+
+-- Check if the user exists, if not create the user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
