85c6533cb55aed4cef87a6f5dd4d4cd39be4d101
DORCAS AUMA <dorcasauma89@gmail.com>
2024-05-22 17:45:45 +0300

    write into a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+
+const fs = require('fs');
+
+
+// Function to write a string to a file
+function writeToFile(filePath, content) {
+    fs.writeFile(filePath, content, 'utf8', (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`Content has been written to ${filePath}`);
+        }
+    });
+}
+
+
+// Check if file path and content are provided as arguments
+if (process.argv.length !== 4) {
+    console.error('Usage: node script.js <file_path> <content>');
+} else {
+    const filePath = process.argv[2];
+    const content = process.argv[3];
+    writeToFile(filePath, content);
+}
