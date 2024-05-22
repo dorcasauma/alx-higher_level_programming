48c30e76f7c86fa755a68a77144d658ad5b560b5
DORCAS AUMA <dorcasauma89@gmail.com>
2024-05-22 17:02:55 +0300

    ff

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,18 +1,17 @@
 #!/bin/bash
 
---Database name passed as argument--
-db_name="hbtn_0c_0"
+--Database name passed as argument
 
-mysql_commands=$(cat <<EOF
-CREATE TABLE IF NOT EXISTS $db_name.second_table (
+USE hbtn_0c_;
+
+SET @table_name = 'second_table';
+CREATE TABLE IF NOT EXISTS @table_name (
     id INT,
-    name VARCHAR(256),
-    score INT
+    name VARCHAR(256)
 );
 
-INSERT INTO $db_name.second_table (id, name, score) VALUES (1, 'John', 10);
-INSERT INTO $db_name.second_table (id, name, score) VALUES (2, 'Alex', 3);
-INSERT INTO $db_name.second_table (id, name, score) VALUES (3, 'Bob', 14);
-INSERT INTO $db_name.second_table (id, name, score) VALUES (4, 'George', 8);
-EOF
-)
+INSERT INTO table_name(id, name, score) VALUES (1, 'John', 10);
+INSERT INTO table_name (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO table_name (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO table_name (id, name, score) VALUES (4, 'George', 8);
+
