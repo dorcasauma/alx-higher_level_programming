87d49871c0394d79eb3737740bf96ab64d3a85ee
DORCAS AUMA <dorcasauma89@gmail.com>
2024-08-16 15:30:40 +0300

    reduce line

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,7 +3,6 @@ word = "Holberton"
 word_first_3 = word[:3]  # First 3 letters
 word_last_2 = word[-2:]  # Last 2 letters
 middle_word = word[1:-1] # Middle part without first and last letters
-
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
