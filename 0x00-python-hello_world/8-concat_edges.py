3d4905ea420d7a51ffc424213b217312bf332cd6
DORCAS AUMA <dorcasauma89@gmail.com>
2024-08-16 15:53:18 +0300

    concatenate

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming"
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str)
+print(str[38:67] + str[107:111]  + " " +  str[0:6])
