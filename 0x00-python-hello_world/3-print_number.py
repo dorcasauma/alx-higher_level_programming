a9cf1be853becba1cfd815be625dc8afd411a934
DORCAS AUMA <dorcasauma89@gmail.com>
2024-08-16 18:26:39 +0300

    correction

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 98
-print(f"{(number)} Battery street")
+print(f"{number} Battery street" if isinstance(number, int) else "ValueError: Unknown format code 'd' for object of type 'str'")
