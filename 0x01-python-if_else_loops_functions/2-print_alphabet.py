50311e45f90060bad9d9133f322f68aad7c185bf
DORCAS AUMA <dorcasauma89@gmail.com>
2024-08-28 15:04:44 +0300

    ascii

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-print(f"abcdefghijklmnopqrstuvwxyz")
+# Use a loop to generate characters and build the final string
+alphabet = ""
+for i in range(97, 123):
+    alphabet += chr(i)  # chr(i) converts ASCII code to character
+
+print(alphabet, end="")  # Print the entire alphabet in one line without a newline at the end
