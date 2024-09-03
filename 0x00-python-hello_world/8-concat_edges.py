35b6012f839db0a832bea63cd172b2598133d856
DORCAS AUMA <dorcasauma89@gmail.com>
2024-09-03 10:34:39 +0300

    coreection

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[38:67]  + str[107:112]  +  str[:6])
+my_string = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+print(my_string[38:67]  + my_string[107:112]  +  my_string[:6])
