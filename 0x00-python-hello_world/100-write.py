10c457d362f9cfa31eb2662342c21c961eaf56ac
DORCAS AUMA <dorcasauma89@gmail.com>
2024-09-03 13:35:31 +0300

    adding

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,7 @@
+import sys
+
+# Write the specified string to standard error
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+
+# Exit the program with a status code of 1
+sys.exit(1)
