51bb8adaf1926744c6fe82757ad8e97565009934
DORCAS AUMA <dorcasauma89@gmail.com>
2024-09-03 10:13:49 +0300

    correction

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str[38:67] + " " + str[107:111] + " " + str[:6])
+
+# Original string (not using string literals directly)
+s = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+
+# Extracting parts of the string
+part1 = s[38:67]        # This will extract "object-oriented programming"
+part2 = s[107:111]      # This will extract "with"
+part3 = s[:6]           # This will extract "Python"
+
+# Concatenating parts and printing the result
+print(part1 + " " + part2 + " " + part3)
